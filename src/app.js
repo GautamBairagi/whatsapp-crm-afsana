cbd01a4864c9cb28d30312cc9a78410950ff45c6
Gautam bairagi <109797346+GautamBairagi@users.noreply.github.com>
2026-06-24 11:30:06 +0530

    asdasd
    ddas

--- src/app.js
+++ src/app.js
@@ -35,7 +35,8 @@ app.use(cors({
     origin: [
         process.env.FRONTEND_URL,
         'http://localhost:3000',
-        'https://whatsapp-crm.kiaantechnology.com'
+        'https://whatsapp-crm.kiaantechnology.com',
+        'https://whatsapp.studyfirstinfo.com'
     ].filter(Boolean),
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
